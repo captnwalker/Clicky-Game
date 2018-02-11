d7e3652a2b6a88c75381b3d974249ace4e40092a
Steve Walker <captnwalker@gmail.com>
2018-02-11 18:53:22 -0500

    adjustments to layout in navJS

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,15 +4,23 @@ import "./Nav.css";
 const Nav = props => (
     <nav id="parallax">
         <ul>
-            <li className="scoreBoard">
+
+
+            {/* <li className="scoreBoard">
                 <a href="/clicky-game/">{props.title}</a>
-            </li>
+            </li> */}
+
+            <li>{props.title}</li>
 
             <li id="rw">{props.rightWrong}</li>
+
+            <div align="center">
 
             <li>Your Score: {props.score}</li>
 
             <li>Your Top Score: {props.topScore}</li>
+
+            </div>
         </ul>
     </nav>
 );
