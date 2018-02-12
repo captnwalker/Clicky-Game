86d47ff690d3f1da61e095ed4d70bf1bc0dfd0c9
Steve Walker <captnwalker@gmail.com>
2018-02-11 21:07:07 -0500

    final tweaks prior to deploy

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,28 +5,21 @@ const Nav = props => (
     <nav id="parallax">
         <ul>
 
-
-            {/* <li className="scoreBoard">
-                <a href="/clicky-game/">{props.title}</a>
-            </li> */}
-
             <div align="center">
 
                 <li className="scoreBoard">
-                <a href="/clicky-game/">{props.title}</a>
-            </li>
+                    <a href="/clicky-game/">{props.title}</a>
+                </li>
 
-                {/* <li className="scoreBoard">{props.title}</li> */}
-
-            <li id="rw">{props.rightWrong}</li>
+                <li id="rw">{props.rightWrong}</li>
 
             </div>
 
             <div id="scores" align="center">
 
-            <li>Your Score: {props.score}</li>
+                <li>Your Score: {props.score}</li>
 
-            <li>Your Top Score: {props.topScore}</li>
+                <li>Your Top Score: {props.topScore}</li>
 
             </div>
         </ul>
